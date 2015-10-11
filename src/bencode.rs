c646ac78a9592ce357bcba85c59a47d9822b7caf
aliang <aliang2344@gmail.com>
2015-10-11 14:11:06 -0400

    adding SizedBuffer parserCombinator

--- src/bencode.rs
+++ src/bencode.rs
@@ -6,9 +6,10 @@ use std::io::prelude::*;
 use std::fs::File;
 use std::path::Path;
 use std::str;
+use std::marker::PhantomData;
 use combine::{spaces, parser, between, many, many1, digit, char, any, string, token, Parser, ParserExt, ParseError};
-use combine::primitives::{State, Stream, ParseResult};
-use combine::combinator::{Between, Token, FnParser};
+use combine::primitives::{State, Stream, ParseResult, Consumed};
+use combine::combinator::{Between, Token, FnParser, satisfy};
 
 //my own bencode stuff!
 //
@@ -37,14 +38,56 @@ fn bencode_integer<I>(input: State<I>) -> ParseResult<i64, I> where I:Stream<Ite
     int.parse_state(input)
 }
 
-fn bencode_string<I>(input: State<I>) -> ParseResult<i32, I> where I:Stream<Item=char> {
-    bencode_string_length_prefix(input)
+fn bencode_string<I>(input: State<I>) -> ParseResult<String, I> where I:Stream<Item=char> {
+    /*
+    
+    let (len, consumed) = match bencode_string_length_prefix(input.clone()) {
+        Ok((len, consumed)) => (len, consumed),
+        e => return e
+    };
+    println!("pos: {}", input.position);
+//    println!("input.input: {}", input.input);
+//    Ok((len, consumed))
+
+    consumed.combine(take(len))*/
+
+    take(4).parse_state(input)
 }
 
 fn bencode_string_length_prefix<I>(input: State<I>) -> ParseResult<i32, I> where I:Stream<Item=char> {
     let many_digit = many1::<String, _>(digit());
     let mut get_len = (many_digit, char(':')).map(|(length, _)| length.parse::<i32>().unwrap());
     get_len.parse_state(input)
+}
+
+fn take <I> (num: i32) -> SizedBuffer<I> where I: Stream<Item=char> {
+    SizedBuffer(num, PhantomData)
+}
+
+#[derive(Clone)]
+pub struct SizedBuffer <I>(i32, PhantomData<I>);
+impl <I> Parser for SizedBuffer<I> where I: Stream<Item=char> {
+    type Input = I;
+    type Output = String; //although maybe its a vec<u8>
+    fn parse_lazy(&mut self, mut input: State<I>) -> ParseResult<String, I> {
+        let start = input.position;
+        let mut vec_buf:Vec<char> = Vec::new();
+        for _ in 0..self.0 {
+            match input.uncons() {
+                Ok((other, rest)) => {
+                    vec_buf.push(other);
+                    input = rest.into_inner();
+                }
+                Err(error) => {
+                    return error.combine(|mut error| {
+                        error.position = start;
+                        Err(Consumed::Consumed(error))
+                    })
+                }
+            };
+        }
+        Ok((vec_buf.into_iter().collect(), Consumed::Consumed(input)))
+    }
 }
 
 fn main () {
@@ -56,7 +99,8 @@ fn main () {
     }
 
     //let result = parser(bencode_integer).parse("i5e");
-    let result = parser(bencode_string).parse("3:sss");
+    //let result = parser(bencode_string).parse("3:sss");
+    let result = parser(bencode_string).parse("3:sssssss");
 
     match result {
         Ok((a, _)) => println!("found {}", a),
