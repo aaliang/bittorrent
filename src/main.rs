9be2a118d22d521171c028ab46d692b57431aa72
aliang <aliang2344@gmail.com>
2015-10-12 22:45:39 -0400

    adding ToMetadata traits and related stuff

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,53 @@
 extern crate bencode;
 
 use std::env;
-use bencode::deserialize_file;
+use std::collections::HashMap;
+use bencode::{deserialize_file, Bencode};
+
+#[derive(Debug)]
+struct Metadata {
+    announce: String,
+}
+
+trait ToMetadata {
+    fn to_metadata (&self) -> Option<Metadata>;
+    fn get_int(&self, key: &str) -> Option<i64>;
+    fn get_string(&self, key: &str) -> Option<String>;
+}
+
+impl ToMetadata for HashMap<String, Bencode> {
+    fn to_metadata (&self) -> Option<Metadata> {
+        let announce = self.get_string("announce").unwrap();
+        Some(Metadata {
+            announce: announce,
+        })
+    }
+
+    fn get_int (&self, key: &str) -> Option <i64> {
+        match self.get(key) {
+            Some(&Bencode::Int(a)) => Some(a),
+            _ => None
+        }
+    }
+
+    fn get_string (&self, key: &str) -> Option <String> {
+        match self.get(key) {
+            Some(&Bencode::String(ref a)) => Some(a.clone()),
+            _ => None
+        }
+    }
+
+}
 
 fn main () {
     let path = env::args().nth(1).unwrap();
-    let cont = deserialize_file(path).unwrap();
-    println!("{:?}", cont);
+    let content = deserialize_file(path).unwrap();
+    let metadata = match *content.first().unwrap() {
+        Bencode::Dict(ref x) => {
+             x.to_metadata()
+        },
+        _ => panic!("something")
+    };
+
+    println!("{:?}", metadata.unwrap());
 }
