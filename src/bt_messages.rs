b048a58c42fd3a2d0d5bc2ed3569f7309cdfe88c
aliang <aliang2344@gmail.com>
2015-10-21 15:31:00 -0400

    adding some more messages

--- src/bt_messages.rs
+++ src/bt_messages.rs
@@ -3,28 +3,57 @@ use std::io::Result;
 
 pub enum Message {
     KeepAlive,
+    Choke,
+    Unchoke,
+    Interested,
+    NotInterested,
+    Have {piece_index: u32},
+    Bitfield(Vec<u8>)
 
 }
 
 ///len_prefix is big endian
 ///might want to use traits instead of returning an enum... haven't decided yet. would save a match
 pub fn decode_message <T> (len_prefix: &[u8; 4], stream: &mut T) -> Message where T:Read {
-    let i: u32 = (
-        len_prefix[3] as u32
-        | ((len_prefix[2] as u32) << 8)
-        | ((len_prefix[1] as u32) << 16)
-        | ((len_prefix[0] as u32) << 24));
+    let i = u8_4_to_u32(len_prefix);
     match i {
         0 => Message::KeepAlive,
-        _ => {
+        len => {
             let mut id_buf = [0; 1];
             let _ = stream.read(&mut id_buf);
-            let id = id_buf[0];
+            match id_buf[0] {
+                0 => Message::Choke,
+                1 => Message::Unchoke,
+                2 => Message::Interested,
+                3 => Message::NotInterested,
+                4 => {
+                    let mut buf = [0; 4];
+                    stream.read(&mut buf);
+                    let piece_index = u8_4_to_u32(&buf);
+                    Message::Have{piece_index: piece_index}
+                },
+                5 => {
+                    let bitfield_length = len - 1;
+                    Message::Bitfield(read_out_variable(stream, bitfield_length as u64))
+                }
+                _ => Message::Choke
+            }
 
-            println!("temp");
-            Message::KeepAlive
         }
     }
+}
+
+fn u8_4_to_u32 (bytes: &[u8; 4]) -> u32 {
+    (bytes[3] as u32
+        | ((bytes[2] as u32) << 8)
+        | ((bytes[1] as u32) << 16)
+        | ((bytes[0] as u32) << 24))
+}
+
+fn read_out_variable <T> (stream: &mut T, num_bytes: u64) -> Vec<u8> where T:Read {
+    let mut buf = Vec::new();
+    stream.take(num_bytes).read(&mut buf);
+    buf
 }
 
 pub fn test () {
